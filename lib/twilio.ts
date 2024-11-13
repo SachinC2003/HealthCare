382b3767e3e59427f67bacf2f185e261d608ce45
SachinC2003 <sachinchougule783@gmail.com>
2024-11-13 12:15:43 +0530

    deployment

--- lib/twilio.ts
+++ lib/twilio.ts
@@ -7,7 +7,7 @@ const client = twilio(accountSid, authToken);
 export const sendOtpToUser = (phoneNumber:any, otp: any) => {
     return client.messages.create({
         body: `Your OTP is ${otp}`,
-        from: process.env.TWILIO_PHONE_NUMBER,
+        from: process.env.TWILIO_PHONE_NUMBER, 
         to: phoneNumber
     });
 };
